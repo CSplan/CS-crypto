59bfa9a65b518e9ba6dd23986b5d95a1809b934c
very-amused <very-amused@pm.me>
2022-11-09 23:49:11 -0500

    Fix test builds

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,18 @@ import typescript from '@rollup/plugin-typescript'
 const tsconfig = 'src/tsconfig.json'
 const external = ['@peculiar/webcrypto']
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: {
-      dir: 'lib',
-      format: 'es'
-    },
-    plugins: [
-      typescript({
-        tsconfig,
-        outDir: 'lib',
-        removeComments: true
-      })
-    ],
-    external
-  }
-]
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'lib',
+    format: 'es'
+  },
+  plugins: [
+    typescript({
+      tsconfig,
+      outDir: 'lib',
+      removeComments: true
+    })
+  ],
+  external
+}
