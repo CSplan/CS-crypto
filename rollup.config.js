f5930f8c99428e6e3683b7a177390d8170f7d179
very-amused <very-amused@pm.me>
2022-11-12 13:08:45 -0500

    Add license preamble to builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
 import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json'
+
+const banner = `/**
+ * @license
+ * cs-crypto v${pkg.version}
+ * MIT License
+ * Copyright (c) 2022 Keith Scroggs, CSplan LLC
+ */\n`
 
 export default {
   input: 'src/index.ts',
   output: {
     dir: 'lib',
-    format: 'es'
+    format: 'es',
+    banner
   },
   plugins: [
     typescript({
