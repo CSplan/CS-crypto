9f6e9f8e8321b70574c9f7514ea236d818562dd0
very-amused <very-amused@pm.me>
2023-09-20 20:22:18 -0400

    Finish test reporter formatting

--- src/test/reporter.ts
+++ src/test/reporter.ts
@@ -1,6 +1,8 @@
 import 'jasmine'
 import 'process'
 
+const failedSuites = new Map<string, boolean>()
+
 const reporter: jasmine.CustomReporter = {
 	jasmineStarted() {
 		console.log('Running tests:\n')
@@ -16,10 +18,19 @@ const reporter: jasmine.CustomReporter = {
 			process.stdout.write(' \x1b[32mOK\x1b[0m\n')
 			return
 		}
+		// Mark the suite as failed if any specs within it fail, mimicking Go's test behavior
+		failedSuites.set((result as jasmine.SpecResult & { parentSuiteId: string })['parentSuiteId'], true)
 		for (const expectation of result.failedExpectations) {
 			process.stdout.write(`\n\t \x1b[31mFAIL\x1b[0m: ${expectation.message}`)
 		}
 		process.stdout.write('\n')
+	},
+	suiteDone(result) {
+		if (result.failedExpectations.length === 0 && !failedSuites.has(result.id)) {
+			process.stdout.write(`\x1b[32m[${result.description}: OK]\x1b[0m\n\n`)
+			return
+		}
+		process.stdout.write(`\x1b[31m[${result.description}: FAIL]\x1b[0m\n\n`)
 	}
 }
 
