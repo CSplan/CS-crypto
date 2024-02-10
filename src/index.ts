3ee507310ebded5d1f2ae654d082edf5474631d1
very-amused <very-amused@pm.me>
2024-02-10 17:21:34 -0500

    Remove ABconcat export

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ export * as rsa from './rsa.js'
 export * as base64 from './base64.js'
 export { binaryConcat } from './binary.js'
 /** @deprecated - Top-level exports have been moved to the base64 module and will soon be removed */
-export { encode, decode, ABconcat } from './base64.js'
+export { encode, decode } from './base64.js'
